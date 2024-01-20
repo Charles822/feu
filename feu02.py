87072aa728591ab584113362acc7b859cc5692e2
Charles822 <55421703+Charles822@users.noreply.github.com>
2024-01-20 13:24:22 +0800

    Update feu02.py

--- feu02.py
+++ feu02.py
@@ -12,7 +12,6 @@ def file_exist(file):
 		raise argparse.ArgumentTypeError(f"error, {file} is not in this directory")
 	
 
-
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Process 2 file names, the board and the shape to find.')
     parser.add_argument('board_file', type=file_exist, help='the board.')
@@ -21,18 +20,9 @@ def parse_arguments():
     return args.board_file, args.shape_file
 
 
-board, shape = parse_arguments()
-
-
 def read_file(file_name):
 	return file_name.read()
-	
 
-board = read_file(board)
-shape = read_file(shape)
-
-print(board)
-print(shape)
 
 def valid_content(file_content):
 	for char in file_content:
@@ -44,10 +34,7 @@ def valid_content(file_content):
 	return True
 
 
-valid_content(board)
-valid_content(shape)
-
-
+# Function to format the content of a file into a 2D list
 def input_formatting(file_content):
 	formatted_input = []
 	temp_row = []
@@ -59,17 +46,8 @@ def input_formatting(file_content):
 		temp_row = []
 	return formatted_input
 
-board_table_sublist = input_formatting(board)
 
-shape_table_sublist = input_formatting(shape)
-
-print(board_table_sublist)
-print(shape_table_sublist)
-
-
-# generate row variations 
-
-# generate the first row variation 
+# Function to create the first variation of the shape's row
 def create_first_variation(row, board_table_sublist):
 	row_variations = []
 	temp_row = []
@@ -87,7 +65,6 @@ def create_first_variation(row, board_table_sublist):
 	return row_variations
 
 
-
 # generate the remaining row variations 
 def generate_other_variations(row_variations, board_table_sublist, shape_table_sublist):
 	dummy_row = ["-" for i in range(len(max(board_table_sublist)))]
@@ -102,8 +79,8 @@ def generate_other_variations(row_variations, board_table_sublist, shape_table_s
 	return row_variations
 
 
-
-def generate_variations(shape_table_sublist):
+# Function to generate all variations for the shape
+def generate_variations(shape_table_sublist, board_table_sublist):
 	variations = {}
 	i = 1
 	for row in shape_table_sublist:
@@ -111,15 +88,10 @@ def generate_variations(shape_table_sublist):
 		row_variations = generate_other_variations(row_variations, board_table_sublist, shape_table_sublist)
 		variations[i] = row_variations
 		i += 1
-		print(row_variations)
 	return variations
 
-dictionnary = generate_variations(shape_table_sublist)
 
-print(f'dictionnary: {dictionnary}')
-
-
-
+# Function to check if a row from the board matches a row variation
 def check_row_equal(item, row):
 	for i in range(0, len(item)):
 		if row[i] == "-" or row[i] == " ":
@@ -131,7 +103,8 @@ def check_row_equal(item, row):
 	return True
 
 
-def shape_in_board(board_table_sublist, dictionnary): 
+# Function to find if the shape is in the board
+def shape_in_board(board_table_sublist, variations): 
 	dummy_row = ["-" for i in range(len(max(board_table_sublist)))]
 	temp_result = []
 	board_result = []
@@ -139,56 +112,48 @@ def shape_in_board(board_table_sublist, dictionnary):
 	for index_board, item in enumerate(board_table_sublist):
 		dict_item = 1
 		
-		for index_shape, row in enumerate(dictionnary[dict_item]): 
+		for index_shape, row in enumerate(variations[dict_item]): 
 			
-			if check_row_equal(item, row) is True and len(dictionnary) == 1:
+			if check_row_equal(item, row) is True and len(variations) == 1:
 				board_result.append(row)
 				diff = len(board_table_sublist) - len(board_result)
 				for i in range(0, diff):
 					board_result.append(dummy_row)
 				return "Trouvé" , board_result
 		
-			elif check_row_equal(item, row) is True and len(dictionnary) > 1:
-				if len(board_table_sublist[board_table_sublist.index(item):]) < len(dictionnary):
+			elif check_row_equal(item, row) is True and len(variations) > 1:
+				if len(board_table_sublist[board_table_sublist.index(item):]) < len(variations):
 					pass
 				else: 
 					temp_result.append(row)
-					print(temp_result)
 					n = 1
-					for _ in range(1, (len(dictionnary) - dict_item) + 1):
-						if check_row_equal(board_table_sublist[index_board + n], dictionnary[dict_item + n][index_shape]) is True:
-							temp_result.append(dictionnary[dict_item + n][index_shape])	
-							print(temp_result)
+					for _ in range(1, (len(variations) - dict_item) + 1):
+						if check_row_equal(board_table_sublist[index_board + n], variations[dict_item + n][index_shape]) is True:
+							temp_result.append(variations[dict_item + n][index_shape])	
 						else:
 							temp_result = [] 
-							print(temp_result)
 						n += 1
-				if temp_result != []:#n == len(dictionnary) - dict_item:
+				if temp_result != []:
 					for row in temp_result:
 						board_result.append(row)
-						print(board_result)
 					diff = len(board_table_sublist) - len(board_result)
 					for i in range(0, diff):
 						board_result.append(dummy_row)
-					return "Trouvé" , board_result
+					return "Trouvé !" , board_result
 		
 		else:
 			board_result.append(dummy_row)
-			print(board_result)
 	return "Introuvable", board_result
 
 
-result, board_result = shape_in_board(board_table_sublist, dictionnary)
+# Function to exit if the shape is not found
+def no_shape(result):
+	if result == "Introuvable":
+		print(result)
+		exit()
 
 
-if result == "Introuvable":
-	print(result)
-	exit()
-else:
-	print(result)
-	print(board_result)
-
-
+# Function to calculate the coordinates of the shape
 def coordinates(board_result):
 	for item in board_result:
 		for element in item:
@@ -201,12 +166,6 @@ def coordinates(board_result):
 		exit()
 	
 
-x, y = coordinates(board_result)
-
-print(f"coordinates are: {x}, {y}")
-
-
-
 def display_board_result(board_result):
 	board_result.reverse()
 	final_result = ""
@@ -217,5 +176,30 @@ def display_board_result(board_result):
 	return final_result[:-1]
 
 
-print(display_board_result(board_result))
+def main():
+	# Handling errors and Parsing
+	board, shape = parse_arguments()
+	board = read_file(board)
+	valid_content(board)
+	shape = read_file(shape)
+	valid_content(shape)
+	board_table_sublist = input_formatting(board)
+	shape_table_sublist = input_formatting(shape)
+
+	
+	# Resolution
+	variations = generate_variations(shape_table_sublist, board_table_sublist)
+	result, board_result = shape_in_board(board_table_sublist, variations)
+	no_shape(result)
+	x, y = coordinates(board_result)
+	final_result = display_board_result(board_result)
+
+	# Display
+	print(*(result, f"coordinates are: {x}, {y}", final_result), sep="\n")
+
+
+if __name__ == "__main__":
+    main()
+
+
 
